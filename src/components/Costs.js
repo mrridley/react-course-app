89a67b7c02e56fcd8acec48272dc42f84c4449cb
max <max.bajandin.work@gmail.com>
2024-08-29 21:14:26 +0500

    Card component to reduce css copypaste

--- src/components/Costs.js
+++ src/components/Costs.js
@@ -1,14 +1,15 @@
 import CostItem from "./CostItem";
 import "./Costs.css";
+import Card from "./Card";
 
 function Costs(props) {
     const costs = props.costs;
     return (
-        <div className='costs'>
+        <Card className='costs'>
             {costs.map( (el) => (
                <CostItem name={el.name} price={el.price} date={el.date}></CostItem>
             ))}
-        </div>
+        </Card>
     );
 }
 

ee3339edaeaba00b590c776ffcc6482df809f03b
max <max.bajandin.work@gmail.com>
2024-09-03 11:46:32 +0500

    homework #2 two-way data binding

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -2,17 +2,21 @@ import CostItem from "./CostItem";
 import "./Costs.css";
 import Card from "../UI/Card";
 import CostsFilter from "./CostsFilter";
+import React, {useState} from "react";
 
 const Costs = (props) => {
+
+    const [year, setYear] = useState('2021');
     const costs = props.costs;
     const changeYearHandler = (year) => {
         console.log(year);
+        setYear(year);
     }
 
     return (
         <div>
             <Card className='costs'>
-                <CostsFilter onChangeYear={changeYearHandler} />
+                <CostsFilter year={year} onChangeYear={changeYearHandler} />
                 {costs.map( (el) => (
                    <CostItem key={el.name} name={el.name} price={el.price} date={el.date}></CostItem>
                 ))}
--- src/components/Costs/CostsFilter.js
+++ src/components/Costs/CostsFilter.js
@@ -10,6 +10,8 @@ const CostsFilter = (props) => {
       <div className="costs-filter__control">
         <label>Выбор По Году</label>
         <select value={props.year} onChange={yearChangeHandler}>
+          <option value="2022">2024</option>
+          <option value="2022">2023</option>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
